3837708ecfa36cba8387cdd4abf395e0e4900a03
MerryChristmas <74279730+JowenPang@users.noreply.github.com>
2020-12-26 00:25:38 +0800

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,46 +1,123 @@
 package monopoly;
 
-public class Board {
-    Square[] square = new Square[32];
-    public Board() {
-        square[0] = new Start();
-        square[1] = new Duo();
-        square[2] = new Tri();
-        square[3] = new Sin();
-        square[4] = new Empty();
-        square[5] = new Duo();
-        square[6] = new Sin();
-        square[7] = new Sin();
-        square[8] = new Shop();
-        square[9] = new Duo();
-        square[10] = new Sin();
-        square[11] = new Duo();
-        square[12] = new Empty();
-        square[13] = new Sin();
-        square[14] = new Sin();
-        square[15] = new Tri();
-        square[16] = new Chest();
-        square[17] = new Sin();
-        square[18] = new Duo();
-        square[19] = new Tri();
-        square[20] = new Empty();
-        square[21] = new Sin();
-        square[22] = new Duo();
-        square[23] = new Sin();
-        square[24] = new Shop();
-        square[25] = new Sin();
-        square[26] = new Duo();
-        square[27] = new Duo();
-        square[28] = new Empty();
-        square[29] = new Tri();
-        square[30] = new Sin();
-        square[31] = new Sin();
-    }
-    public void play(Player player) {
-        int nextPosition=player.getPosition();
-        Square nextSquare=square[nextPosition];
-        nextSquare.event(player);
+import java.util.Scanner;
 
+public class Board {
+    private int currentTurn=0;
+    Square[] square = new Square[32];
+    Player [] player;
+    Scanner sc=new Scanner(System.in);
+
+    public Board(int noOfPlayer) {
+        player =new Player[noOfPlayer];
+
+        System.out.println("Enter character to represent each player");
+        for (int i = 0; i < player.length; i++) {
+            char name=sc.next().charAt(0);
+            player[i]=new Player(name);      //create each new player
+            player[i].firstDiceRoll=player[i].rollDice();
+            System.out.println( "first dice roll: "+ player[i].firstDiceRoll);
+        }
+        checkSequence(player);
+        for (int i = 0; i < player.length; i++) {
+            System.out.print(player[i].name + " ");  //to check the sequence of player
+        }
+
+        for (int i = 0; i < square.length; i++) {
+            if (i == 0)
+                square[i] = new Start("Start");
+            else if (i == 16)
+                square[i] = new Chest("Chest");
+            else if (i == 8 || i == 24)
+                square[i] = new Shop("Shop");
+            else if (i == 4 || i == 12 || i == 20 || i == 28)
+                square[i] = new Empty("    ");
+            else if (i == 2 || i == 15 || i == 19 || i == 29)
+                square[i] = new Tri("Tri-M");
+            else if (i == 1 || i == 5 || i == 9 || i == 11 || i == 18 || i == 22 || i == 26 || i == 27)
+                square[i] = new Duo("Duo-M");
+            else
+                square[i] = new Sin("Sin-M");
+
+        }
+    }
+
+    public Square play(Player player) {
+        int a=player.rollDice();
+        System.out.println("Player's dice: "+ a);
+        int nextPosition=(player.getPosition()+a)%32;
+        player.setPosition(nextPosition);
+        System.out.println("Player's next position: "+ nextPosition);
+        square[nextPosition].event(player);
+        return square[nextPosition];
+
+    }
+
+    public void printBoard(Player player) {
+        System.out.println(" ----------------------------------------------------------------------");
+        for (int m = 16; m < 25; m++) {
+            System.out.print(" | " + square[m].name);
+        }
+        System.out.println("|");
+        for (int m = 16; m< 25; m++) {
+            System.out.print(" | ");
+            for (int n = 0; n< square[m].name.length(); n++) {
+                if(n==0) {
+                    if (player.getPosition() == m)
+                        System.out.print(player.name);
+                    else
+                        System.out.print(" ");
+                }
+                else
+                    System.out.print(" ");
+            }
+        }System.out.println("|");
+        System.out.println(" ----------------------------------------------------------------------");
+
+        System.out.println(" ----------------------------------------------------------------------");
+        for (int m = 8; m >=0; m--) {
+            System.out.print(" | " + square[m].name);
+        }
+        System.out.println("|");
+        for (int m = 8; m >=0; m--) {
+            System.out.print(" | ");
+            for (int n = 0; n< square[m].name.length(); n++) {
+                if(n==0) {
+                    if (player.getPosition() == m)
+                        System.out.print(player.name);
+                    else
+                        System.out.print(" ");
+                }
+                else
+                    System.out.print(" ");
+            }
+        }System.out.println("|");
+        System.out.println(" ----------------------------------------------------------------------");
+    }
+    private static Player [] checkSequence(Player[] player) {
+        //decide sequence
+        for (int pass = 0; pass<player.length-1; pass++) {
+            for (int i = 0; i < player.length-1; i++) {
+                Player temp;
+                if(player[i].firstDiceRoll<player[i+1].firstDiceRoll) {
+                    temp = player[i];            //swap between player , swap whole object
+                    player[i] = player[i + 1];
+                    player[i + 1] = temp;
+                }
+            }
+
+        }return player;
+    }
+    public Player getCurrentPlayer(){
+        return player[currentTurn];
+    }
+
+    public int getCurrentTurn() {
+        return currentTurn;
+    }
+
+    public void setCurrentTurn(int currentTurn) {
+        this.currentTurn = currentTurn;
     }
 }
 
