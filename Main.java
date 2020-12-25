b8657c3f12b7d2b77d4510b6e155b224ef9c492a
MerryChristmas <74279730+JowenPang@users.noreply.github.com>
2020-12-26 00:27:48 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 package monopoly;
+package monopoly;
 
 import java.util.Scanner;
 
@@ -7,42 +8,21 @@ public class Main {
         Scanner sc=new Scanner(System.in);
         System.out.println("Number of player:");
         int n=sc.nextInt();
+        Board board=new Board(n);
 
         Player [] player=new Player[n];  //Object Array
         Monsters monster =new Monsters();
-        Board board=new Board();
 
-
-        System.out.println("Enter character to represent each player");
-        for (int i = 0; i < player.length; i++) {
-            char name=sc.next().charAt(0);
-            player[i]=new Player(name);      //create each new player
-            player[i].firstDiceRoll=player[i].rollDice();
-            System.out.println( "first dice roll: "+ player[i].firstDiceRoll);
-        }
-        checkSequence(player);
-        for (int i = 0; i < player.length; i++) {
-            System.out.print(player[i].name + " ");  //to check the sequence of player
-        }
+        //the game will continue loop in this do while loop
         do {
-            board.play(player[0]);
-        }while(n!=0);
+            for (int i = 0; i <player.length ; i++) {
 
-
-    }
-
-    private static Object [] checkSequence(Player[] player) {
-        //decide sequence
-        for (int pass = 0; pass<player.length-1; pass++) {
-            for (int i = 0; i < player.length-1; i++) {
-                Player temp;
-                if(player[i].firstDiceRoll<player[i+1].firstDiceRoll) {
-                    temp = player[i];            //swap between player , swap whole object
-                    player[i] = player[i + 1];
-                    player[i + 1] = temp;
-                }
+                board.setCurrentTurn(i);
+                System.out.println("\nNow is player "+ (i+1)+ " turn");
+                board.play(board.getCurrentPlayer());
+                board.printBoard(board.getCurrentPlayer());
+                //will need special condition to break the loop
             }
-
-        }return player;
+        }while(n!=0);
     }
 }
