5bf4364043a0bd70e64021c9b26e67593a9a8e4f
Jowen <74279730+JowenPang@users.noreply.github.com>
2020-12-27 11:18:07 +0800

    Update Role.java

--- Role.java
+++ Role.java
@@ -13,4 +13,60 @@ public abstract class Role {
 
     public abstract void flee();
 
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level += level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp += hp;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = +strength;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence += defence;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility += agility;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp += exp;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold += gold;
+    }
 }
+
