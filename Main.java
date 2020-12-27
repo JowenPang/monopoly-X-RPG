36150dc8ed5f2ef2d21a3ec21a97e25c603bef95
Jowen <74279730+JowenPang@users.noreply.github.com>
2020-12-27 22:19:46 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 package monopoly;
-package monopoly;
 
 import java.util.Scanner;
 
@@ -14,29 +13,28 @@ public class Main {
         Monsters monster =new Monsters();
 
         //the game will continue loop in this do while loop
+
+        board.printBoard();
         do {
-            for (int i = 0; i <player.length ; i++) {
-                board.setCurrentTurn(i);
-                System.out.println("\nNow is player "+ (i+1)+ " turn");
-                System.out.println("Option: 1.Roll Dice  2.Check Stats  3.Quit the Game");
-                int choice= sc.nextInt();
-                if(choice==1)
-                    board.play(board.getCurrentPlayer());
-                else if (choice==2) {
-                    System.out.println(board.getCurrentPlayer().toString());
-                    System.out.println("Continue to roll dice? 1. Yes 2. No");
-                    int choice2= sc.nextInt();
-                    if(choice2==1)
-                        board.play(board.getCurrentPlayer());
-                    else
-                        System.out.println("your turn is skip");
-                }
+            board.setCurrentTurn(1);
+            System.out.println("\nNow is player "+ board.getCurrentPlayer().getName() + " turn");
+            System.out.println("Option: 1.Roll Dice  2.Check Stats  3.Quit the Game");
+            int choice= sc.nextInt();
+            if(choice==1)
+                board.play();
+            else if (choice==2) {
+                System.out.println(board.getCurrentPlayer().toString());
+                System.out.println("Continue to roll dice? 1. Yes 2. No");
+                int choice2= sc.nextInt();
+                if(choice2==1)
+                    board.play();
                 else
-                    System.out.println("Player "+ (i+1) + "left the game");
-
-
-                //will need special condition to break the loop
+                    System.out.println("your turn is skip");
             }
+            else
+                System.out.println("Player "+ board.getCurrentTurn() + "left the game");
         }while(n!=0);
+
+
     }
 }
