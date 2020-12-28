0648ce99a27e8befc0e7662145ffd8c5e9b78f99
ToughestNailEver <76063348+ToughestNailEver@users.noreply.github.com>
2020-12-28 19:13:03 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args){
+        Instructions.help();
         Scanner sc=new Scanner(System.in);
         System.out.println("Number of player:");
         int n=sc.nextInt();
