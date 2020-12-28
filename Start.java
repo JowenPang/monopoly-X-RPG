afcb65b2db24226bd1c8df1e8fcc18c58317e8df
Jowen <74279730+JowenPang@users.noreply.github.com>
2020-12-29 00:05:03 +0800

    Update Start.java

--- Start.java
+++ Start.java
@@ -1,25 +1,23 @@
 
 
-public class Start {
-    int level, hp, strength,defence, agibility;
-    
-    
-    public void Start(){
-        System.out.println("You are now at Start");
-        System.out.println("");
-        level+=1;
-        hp+=20;
-        strength+=25;
-        defence+=10;
-        agibility+=10;
-        System.out.println("You current level: " + level);
-        System.out.println("Your current hp: " + hp);
-        System.out.println("Your current strength: " + strength);
-        System.out.println("Your current defence: " + defence);
-        System.out.println("Your current agibility: " +agibility);
-        System.out.println("");
-        System.out.println("Continue enjoy the game!");
-        
+package monopoly;
+class Start extends Square {
+
+    public Start(String name) {
+        this.name = name;
     }
-    
+
+    public void event(Player player){
+        System.out.println("You made it through one round, congratulations!");
+        System.out.println("");
+        player.setLevel(1);
+        player.setHp(20);
+        player.setStrength(25);
+        player.setDefence(10);
+        player.setAgility(10);
+        System.out.println("Your new statistics: "+ player.toString());
+        System.out.println("Continue enjoy the game!");
+
+    }
+
 }
