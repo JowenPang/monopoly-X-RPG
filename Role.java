00a63600cb92fe852cf2a1ea1f1647a789fd1f6f
Jowen <74279730+JowenPang@users.noreply.github.com>
2020-12-29 11:30:42 +0800

    Update Role.java

--- Role.java
+++ Role.java
@@ -4,7 +4,7 @@ public abstract class Role {
     int level,hp,strength,defence, agility,exp,gold;
 
     public Role() {
-        level=1; hp=25; strength=5; defence=5; agility=5; exp=5;
+        level=1; hp=25; strength=5; defence=5; agility=5; exp=0;
         gold=200;
     }
     public abstract void attack();
