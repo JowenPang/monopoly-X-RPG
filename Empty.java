d67f859aaa3dc69c2520cfe1f9af0234fde147fb
ToughestNailEver <76063348+ToughestNailEver@users.noreply.github.com>
2020-12-30 11:09:53 +0800

    Update Empty.java

--- Empty.java
+++ Empty.java
@@ -10,7 +10,7 @@ public class Empty extends Square{
         System.out.println("Option: 1.Heal yourself  2.Do nothing");
         System.out.print("Your option: ");
         int option=sc.nextInt();
-        System.out.println("");
+        System.out.println();
         
      
         if(option==1){
@@ -18,25 +18,26 @@ public class Empty extends Square{
             System.out.print("Choose the healing item that you would like to use: ");
             sc.nextLine();
             String healingItem=sc.nextLine();
+            boolean buy = false;
             for(String item: player.getItem()){
-              if(healingItem.equalsIgnoreCase("Hi-Potion")){
-                player.setHp(15);
-                player.setStrength(15);
-                player.item.remove("Hi-potion");
-                System.out.println("You successfully used Hi-Potion to heal yourself");
-                System.out.println();
-                System.out.println("Current item that you have: " + player.getItem());
-               }
-                   
-              break;
-        // } else{
-          //   System.out.println("You choose to do nothing");
+                if(healingItem.equalsIgnoreCase("Hi-Potion")){
+                    player.setHp(15);
+                    player.setStrength(15);
+                    player.item.remove("Hi-potion");
+                    System.out.println("You successfully used a Hi-Potion to heal yourself.\n");
+                    System.out.println("Current item that you have: " + player.getItem()); // show remaining in backpack
+                    buy = true;
+                    break;
+                }
+            }
+            if (!buy){
+                System.out.println("Oops! You don't have current item to heal yourself yet.");
+            }
+        } else {
+             System.out.println("You choose to do nothing");
         }
-           System.out.println("You don't have item to heal yourself yet");
-    }
         System.out.println("Your current hp: " + player.getHp());
         System.out.println("Your current strength: " + player.getStrength()); 
         System.out.println("Good Day!");
+    }
 }
-}
-
