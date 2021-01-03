cac4e00a6a567baa50ca3534681547aa81ed9c23
JOWEN <jowenpg@gmail.com>
2021-01-03 13:29:58 +0800

    Added git function

--- src/monopoly/Chest.java
+++ src/monopoly/Chest.java
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 package monopoly;
-
 import java.util.Random;
 
-class Chest extends Square{
-=======
-import java.util.Random;
-
- public class Chest extends Square{
->>>>>>> origin/master
+public class Chest extends Square{
     public Chest(String name) {
         this.name = name;
     }
@@ -16,10 +9,10 @@ import java.util.Random;
     public void event(Player player){
         Random r= new Random();
         String rare;
-<<<<<<< HEAD
         String[]rareItem={"Master Sword","Blade of Chaos","Key Blade","Hidden Blade","Gravity Gun"};
         System.out.println("You can open a treasure chest");
-        System.out.println("\nChoose 1 or 2: ");
+        System.out.println("");
+        System.out.println("Choose 1 or 2: ");
         int choose =sc.nextInt();
 
         switch(choose){
@@ -60,51 +53,3 @@ import java.util.Random;
     }
 
 }
-=======
-        String[]rareItem={"Master Sword","Blade of Chaos","Key Blade","Hidden Blade","Gravity Gun"};     
-        System.out.println("You can open a treasure chest");
-        System.out.println("");
-        System.out.println("Choose 1 or 2: ");
-        int choose =sc.nextInt();
-        
-        switch(choose){
-            case 1:
-                   int gold =r.nextInt(101);
-                   player.setGold(gold);
-                   System.out.println("Congratulations! You got " + gold + " gold as reward");
-                   break;
-        
-            case 2:
-                   rare=rareItem[r.nextInt(rareItem.length)]; //randomise the index
-                   if(rare==rareItem[0]){
-                         //player.setHp(2);
-                         //player.setStrength(5);
-                         player.item.add("Master Sword");
-                    }else if (rare==rareItem[1]){
-                         //player.setHp(3);
-                         //player.setStrength(4);
-                         player.item.add("Blade of Chaos");
-                    }else if (rare==rareItem[2]){
-                         //player.setHp(5);
-                         //player.setStrength(4);
-                         player.item.add("Key blade");
-                    }else if(rare==rareItem[3]){
-                         //player.setHp(6);
-                         //player.setStrength(6);
-                         player.item.add("Hidden Blade");
-                    }else{
-                         //player.setHp(7);
-                         //player.setStrength(8);
-                         player.item.add("Gravity Gun");
-                    }
-                   System.out.println("Congratulations! You got " + rare +" as reward");
-                   break;
-        }
-       
-  
-    }
-
-}
-    
-
->>>>>>> origin/master
